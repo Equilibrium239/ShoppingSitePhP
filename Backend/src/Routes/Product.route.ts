7f117b8a1f88d48f85ce1841aec677d247596464
Equilibrium <miltos-05032006@hotmail.com>
2026-04-14 14:32:18 +0200

    added so that errors pop up if you are trying to get an product that dosent exist

--- Backend/src/Routes/Product.route.ts
+++ Backend/src/Routes/Product.route.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { getAllProducts } from "../models/Products";
+import { getAllProducts, getProductByID, updateProduct } from "../models/Products";
 
 
 const ProductRouter = Router();
@@ -9,6 +9,37 @@ ProductRouter.get("/", async (req: Request, res: Response) => {
     const products = await getAllProducts();
     res.status(200).json(products);
 });
+
+
+ProductRouter.get("/:id", async (req: Request, res: Response) => {
+    const ProductId = parseInt(req.params.id as string);
+    const Product = await getProductByID(ProductId);
+
+    if (Product) {
+        res.status(200).json({Product});
+    } else {
+        return res.status(404).json({message: "Product Not Found"});
+    }
+});
+
+
+ProductRouter.put("/:id", async (req: Request, res: Response) => {
+    const ProductId = parseInt(req.params.id as string);
+    const updateData = req.body;
+
+    try {
+        const updatedProduct = await updateProduct(ProductId, updateData);
+        if (!updateProduct) {
+            return res.status(404).json({message: "Product Not Found"});
+        }
+
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: "Something When Wrong" });
+    }
+});
+
+
 
 
 
