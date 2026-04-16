4ec5324968b0f3ac3b2f2ccd1b3e2445feca651f
Equilibrium <miltos-05032006@hotmail.com>
2026-04-16 08:29:02 +0200

    made a mini menu for becoming a member

---
+++ my-app/components/Medlemskap.php
@@ -0,0 +1,29 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bli Medlem</title>
+</head>
+<body>
+    <h2>bli Medlem</h2>
+
+    <form action="register.php" method="POST">
+        <label for="name">Namn:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Lösenord:</label><br>
+        <input type="password" name="password" required><br><br>
+    </form>
+
+
+    
+</body>
+</html>
+
