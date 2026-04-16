a4837db88a6dcead10fb1759fa72e0d585f8ff19
Equilibrium <miltos-05032006@hotmail.com>
2026-04-16 08:41:54 +0200

    unkownn

--- my-app/components/Medlemskap.php
+++ my-app/components/Medlemskap.php
@@ -1,4 +1,30 @@
-<?php 
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bli Medlem</title>
+</head>
+<body>
+    <h2>bli Medlem</h2>
+
+    <form action="register.php" method="POST">
+        <label for="name">Namn:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Lösenord:</label><br>
+        <input type="password" name="password" required><br><br>
+    </form>
+
+
+    <?php 
 
     $host = 'localhost';
     $dbname = 'Shopping';
@@ -34,29 +60,6 @@
 
 ?>
 
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bli Medlem</title>
-</head>
-<body>
-    <h2>bli Medlem</h2>
-
-    <form action="register.php" method="POST">
-        <label for="name">Namn:</label><br>
-        <input type="text" id="name" name="name" required><br><br>
-
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
-
-        <label for="password">Lösenord:</label><br>
-        <input type="password" name="password" required><br><br>
-    </form>
 
 
     
